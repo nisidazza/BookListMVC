13fd8f0cc71fafbabb5ed85edd064cd09dca3b02
Nisida Azzalini <nisida.azzalini@gmail.com>
2020-07-06 20:23:24 +1200

    install Microsoft.AspNetCore.Mvc.Razor.RuntimeCompilation - in case of modification there is no need to restart the application - and add it Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -27,7 +27,7 @@ namespace BookListMVC
         {
             services.AddDbContext<ApplicationDbContext>(
                 options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
-            services.AddControllersWithViews();
+            services.AddControllersWithViews().AddRazorRuntimeCompilation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
